96e0b944dd9c250f782c4ec5d483d5c6865408fd
asif_k3 <51926489+sniprmsntr@users.noreply.github.com>
2023-04-09 00:47:32 -0500

    Add files via upload

---
+++ java-first-program-master/src/main/java/com/h2/App.java
@@ -0,0 +1,23 @@
+package com.h2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static int doubleTheNumber(int number) {
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+}
